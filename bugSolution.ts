7b97134c4fc7761a7dbbe9d539af460710b18925
BugHunter-X <askzenai@gmail.com>
2025-02-08 07:26:04 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,9 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+let num1:number = 1;
+let num2:number = 2;
+
+let result = add(num1, num2); // Correct: both arguments are numbers
+console.log(result); 
